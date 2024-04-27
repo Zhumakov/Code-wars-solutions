4fcb923ed101be59da77b7cc17e23cd443eb0008
Zhumakov <zhumakov06@mail.ru>
2024-04-27 23:33:16 +0500

    Added solution to kata Next bigger number with the same digits

---
+++ Next bigger number.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/55983863da40caa2c900004e
+def find_nearest_bigger(n):
+    num_str = list(str(n))
+    for i in range(-1, -len(num_str) - 1, -1):
+        for j in range(-1, i, -1):
+            if num_str[i] < num_str[j]:
+                num_str[i], num_str[j] = num_str[j], num_str[i]
+                return num_str, i
+    return None, None
+
+
+def next_bigger(n):
+    num_str, i = find_nearest_bigger(n)
+    if num_str is None:
+        return -1
+
+    res = num_str[-len(num_str):i + 1] + sorted(num_str[i + 1:])
+
+    return int(''.join(res))
